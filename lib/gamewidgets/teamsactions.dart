1318fe06b61c09ea4315a774f238cf649c01bac6
Julianafrego <julianafgomes@aluno.uespi.br>
2024-09-28 19:28:49 -0300

    add widgets ao doc teamactions

--- lib/gamewidgets/teamsactions.dart
+++ lib/gamewidgets/teamsactions.dart
@@ -1,33 +1,105 @@
 import 'package:flutter/material.dart';
-import 'package:flutter/services.dart';
+import '../main.dart';
+import 'gamebuttons.dart';
 
-class TeamActionsWidget extends StatelessWidget {
-  const TeamActionsWidget({super.key});
+class TeamActionsListAWidget extends StatelessWidget {
+  const TeamActionsListAWidget({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Column(
+    return const Column(
+      mainAxisSize: MainAxisSize.min, // Isso ajudará a evitar tamanho zero
+
       children: [
-        Expanded(
-          // expanded para que ocupe o maximo de espaço possivel
-          flex: 1,
-          child: Container(
-            color: Colors.blue,
-            child: const Column(
-              children: [],
-            ),
-          ),
+        TeamActionAWidget(
+          actionName: 'Ace',
         ),
-        Expanded(
-          flex: 2,
-          child: Container(
-            color: Colors.yellow,
-            child: const Column(
-              children: [],
-            ),
-          ),
+        TeamActionAWidget(
+          actionName: 'Ataque',
+        ),
+        TeamActionAWidget(
+          actionName: 'Bloqueio',
+        ),
+        TeamActionAWidget(
+          actionName: 'Erro',
         ),
       ],
+    );
+  }
+}
+
+class TeamActionAWidget extends StatelessWidget {
+  final String actionName;
+  const TeamActionAWidget({super.key, required this.actionName});
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.symmetric(vertical: 3.0),
+      child: Row(
+        children: [
+          const AddButtonTeam(),
+          Text(
+            actionName,
+            style: const TextStyle(
+              fontSize: 20,
+              color: MyColors.branco,
+            ),
+          )
+        ],
+      ),
+    );
+  }
+}
+
+class TeamActionsListBWidget extends StatelessWidget {
+  const TeamActionsListBWidget({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Column(
+      mainAxisSize: MainAxisSize.min, // Isso ajudará a evitar tamanho zero
+      mainAxisAlignment: MainAxisAlignment.end,
+      crossAxisAlignment: CrossAxisAlignment.end,
+      children: [
+        TeamActionBWidget(
+          actionName: 'Ace',
+        ),
+        TeamActionBWidget(
+          actionName: 'Ataque',
+        ),
+        TeamActionBWidget(
+          actionName: 'Bloqueio',
+        ),
+        TeamActionBWidget(
+          actionName: 'Erro',
+        ),
+      ],
+    );
+  }
+}
+
+class TeamActionBWidget extends StatelessWidget {
+  final String actionName;
+  const TeamActionBWidget({super.key, required this.actionName});
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.symmetric(vertical: 3.0),
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.end,
+        children: [
+          Text(
+            actionName,
+            style: const TextStyle(
+              fontSize: 20,
+              color: MyColors.branco,
+            ),
+          ),
+          const AddButtonTeam(),
+        ],
+      ),
     );
   }
 }
